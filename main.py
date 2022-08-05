ae1a2e1c961cba609664519d3c185a62cb49f64f
PI <jatin241001@gmail.com>
2022-08-05 22:31:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ from tkinter import *
 root= Tk()
 #Basic GUI and root is made
 #GUI LOGIC
+#Width x height
+
+root.geometry(" 4195x61681")
+root.minsize("400x400")
+# root.maxsize("10000x10000")
+#this will lock the max size so you can  not drag the box to increase its size
+#the same thing is applicable for minsize
+inpt= Label (text="input")
+inpt.pack()
+
 root.mainloop()  
