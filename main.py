6e5222352b98d02eb9038c962a905cf3beebd7f5
PI <jatin241001@gmail.com>
2022-08-08 22:11:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ root= Tk()
 #GUI LOGIC
 #Width x height
 
-root.geometry(" 4195x61681")
-root.minsize("400x400")
+#root.geometry(" 4195x61681")
+#root.minsize("400x400")
 # root.maxsize("10000x10000")
 #this will lock the max size so you can  not drag the box to increase its size
 #the same thing is applicable for minsize
