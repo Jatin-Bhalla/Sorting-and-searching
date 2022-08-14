de6f096752ba922dadd3b4a3aa27105f80f1be70
PI <jatin241001@gmail.com>
2022-08-14 23:47:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,14 +82,41 @@ def Bubble_Sort(array):
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
-def Quick_Sort():
+#QUICK SORT
+def partition(array, l, r):
+
+  # choose the rightmost element as pivot
+  pivot = array[r]
+  # pointer for greater element
+  i = l - 1
+  # traverse through all elements
+  # compare each element with pivot
+  for j in range(l, r):
+    if array[j] <= pivot:
+      # if element smaller than pivot is found
+      # swap it with the greater element pointed by i
+      i = i + 1
+      # swapping element at i with element at j
+      (array[i], array[j]) = (array[j], array[i])
+
+  # swap the pivot element with the greater element specified by i
+  (array[i + 1], array[r]) = (array[r], array[i + 1])
+
+  # return the position from where partition is done
+  return i + 1
+   
+def Quick_Sort(array , l ,r):
+     if  l>=r:return 
+     p=partition(array ,l,r)
+     Quick_Sort(array ,l,p-1)
+     Quick_Sort(array,p+1,r)
      # well think of quick aort a bit like divide and conquer ! well conquer the problem
      #break the array into 2 sub arrays and a centre 'pivot' 
       # one array is consisting of values>pivot
       # another has values < pivot
   
 # now call the same funciton to  do the same  on both and further and further till a sorted array forms.
-
+#there will be 2 functions used here
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
 def Submit(): pass
 def Delete():entry.delete(0,END)
