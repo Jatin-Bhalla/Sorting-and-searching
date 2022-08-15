de9754762a01253b95219454ba1617690a9318dc
PI <jatin241001@gmail.com>
2022-08-15 22:32:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from tkinter import *
 #import tkinter
-window= Tk()
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------
+window = Tk() 
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#BASIC functions
+def asc():pass
+def desc():pass
+def submit(): pass
+def submit_query():pass
+def delete():entry.delete(0,END)
+def backspace():entry.delete(len(entry.get())-1 ,END)
+def delete_exit():Exit.delete(0,END)
+def delete_query():entry.delete(0,END)
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# DSA funcitons
 def linear_search(arr, x):
-     for i in range(len(arr)):
+    for i in range(len(arr)):
          if arr[i] == x:
             return i
           #return arr[i]
- 
     return -1
 #----  ------ ------ ----- ----- ---- ----- ---- ---- ---- ---- ---- ----- ----- ----- ----- ----- ----- ----- ----- ---- ----- ---- ----- ----- ----- ---- ---- 
 """ # another linear search code:
@@ -32,20 +42,13 @@ def binary_search(arr , target):
  # if that is the target we use its value as the output of the function
   if arr[m]==target: return m
  #otherwise
-# L--------M---------R
-# L<-----(T<M)---->M<------(T>M)---->R
+# L--------M---------R# L<-----(T<M)---->M<------(T>M)---->R
   if target<arr[m]:r=m-1
     #L<------>r-M______R
-  else: l=m+1
-    #L_____M-l<----->R
+  else: l=m+1    #L_____M-l<----->R
   return -1
 #IF NONE THEN RETURN
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
-# time complexity O(n*n)
-#sorting by finding min_index
-#The basic thing about selection sort is to look at 2 consecutive values and then compare for which is bigger or smaller
-#if asc: it sees 2 values compares and then tells which is smaller and swaps it with the bigger value then the same loop continues till the value it is sorted
-# if the value is smaller then it compares it with the next value all the same while identifying it as minimum
 def selection_sort(array, size):
     #array name , size of the array
     for value in range(size):
@@ -63,6 +66,7 @@ def selection_sort(array, size):
         (array[value], array[min_index]) = (array[min_index], array[value])
           #this is a basic swap code piece(x,y)=(y,x) 
           #the akshay kumar meme
+# time complexity O(n*n)  #sorting by finding min_index#The basic thing about selection sort is to look at 2 consecutive values and then compare for which is bigger or smaller#if asc: it sees 2 values compares and then tells which is smaller and swaps it with the bigger value then the same loop continues till the value it is sorted# if the value is smaller then it compares it with the next value all the same while identifying it as minimum
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
 def bubble_sort(array):
   #instead of n or another variable with len(array) we directly use it
@@ -79,8 +83,6 @@ def bubble_sort(array):
         # swapping elements if elements
         # are not in the intended order
         (array[j] , array[j + 1])=(array[j + 1],array[j])
-
-
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------
 #QUICK SORT
 def partition(array, l, r):
@@ -114,132 +116,62 @@ def quick_sort(array , l ,r):
      #break the array into 2 sub arrays and a centre 'pivot' 
       # one array is consisting of values>pivot
       # another has values < pivot
-  
-# now call the same funciton to  do the same  on both and further and further till a sorted array forms.
+ # now call the same funciton to  do the same  on both and further and further till a sorted array forms.
 #there will be 2 functions used here
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------
-def display_1():
-    # .get is used to obtain the current value
-    # of entry_1 widget (This is always a string)
-    print(entry_1.get())
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def display_2():
-    num = entry_2.get()
-    # Try convert a str to int
-    # If unable eg. int('hello') or int('5.5')
-    # then show an error.
-    try:
-       num = int(num)
-    # ValueError is the type of error expected from this conversion
-    except ValueError:
-        #Display Error Window (Title, Prompt)
-        showerror('Non-Int Error', 'Please enter an integer')
-    else:
-        print(num)
-
-def display_3():
-    # Ask Integer Window (Title, Prompt)
-    # Returned value is an int
-    ans = askinteger('Enter Integer', 'Please enter an integer')
-    # If the user clicks cancel, None is returned
-    if ans is not None:
-        print(ans)
-def asc(): pass
-def desc(): pass
-def submit(): pass
-def delete():entry.delete(0,END)
-def backspace():entry.delete(len(entry.get())-1 ,END)
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------
-#Basic GUI and window is made
-#change the size of the window
-#Width x height
-
-window.geometry("1024x856")
+window.geometry("1200x800")
 window.title("Algo Analyzr")
+
 #this will lock the max size so you can  not drag the box to increase its size
 #the same thing is applicable for minsize
-window.minsize(600,500)
+window.minsize(600,600)
 window.maxsize(10000,10000)
-#background colour
-window.config(background="#C0A9BD")#enter a colour or a hex value
-#----------------
-# Entry widget =textbox to accept an input from the user
-
-entry= Entry (window,font=("Arial",50))
-entry.pack(side=LEFT )
-# ------------------
-entry_1 = tk.Entry(window)
-btn_1 = tk.Button(window, text = "Display Text", command = display_1)
-
-entry_2 = tk.Entry(window)
-btn_2 = tk.Button(window, text = "Display Integer", command = display_2)
-
-btn_3 = tk.Button(window, text = "Enter Integer", command = display_3)
-
-# Grid is used to add the widgets to window
-# Alternatives are Pack and Place
-entry_1.grid(row = 0, column = 0)
-btn_1.grid(row = 1, column = 0)
-entry_2.grid(row = 0, column = 1)
-btn_2.grid(row = 1, column = 1)
-
-btn_3.grid(row = 2, column = 0)
-btn_4.grid(row = 2, column = 1)
-#-------------------
-# BUTTONS= performs a specific funtion given by the command or linked using it
-backspace_button =Button(window,text='backspace',command = backspace)
-backspace_button.pack(side=RIGHT)
-
-delete_button =Button(window,text='DELETE',command =delete)
-delete_button.pack(side=RIGHT)
-
-submit_button =Button(window,text='SUBMIT',command = submit)
-submit_button.pack(side=RIGHT)
-
-
-# -------------------
-#add an image to the project folder and link them
+window.config(background="#D8DFEE")
+# ----------
+#icon image
 icon = PhotoImage(file='logo.png')
 window.iconphoto(True,icon)
-#label is aarea holding image or text
-inpt= Label(window,
-            text="Input",
-            font=('Arial',16,'bold'),
-            fg='',""" text colour"""
-            bg=''  """colour of the baground behind text"""
-            )
-#https://www.color-hex.com/
-#colour value in hexadecimal
-inpt.pack()
+# ------------------
+#  INNPUT OR OUTPUT DISPLAY
+# LABEL = text on GUI
+# BUTTONS= performs a specific funtion given by the command or linked using it
+# ------------------
+#enter input
+inptt= Label(window,text = "INPUT" ,font=('Arial',18,'bold'), bg ="#D8DFEE" , fg="#647295")
+inptt.pack(side= LEFT,anchor ="nw")
+entry= Entry (window,font=("Arial",18),bg="#A3B4CB",fg="#F4F2F3")
+entry.pack( side= LEFT ,anchor ="nw")
+backspace_button =Button(window,text='BACKSPACE',command = backspace,bg ="#F2E9EB",fg="#647295")
+backspace_button.pack( side= RIGHT,anchor ="ne")
 
-#button: click it and it does action
-button =Button.(window,
-                text="Input",
-                command =click,
-                font=("Arial",16,'bold'),
-                fg='',""" text colour"""
-                bg=''  """colour of the baground behind text""",
-                activeforeground="#00FF00",
-                activebackground="BLACK"
-                state =ACTIVE )
-button.pack()
-#------------------------------------------------------------------------------------------------------------
-#check buttons
-#probably add fg and bg colour and active foreground and active background
+delete_button =Button(window,text='DELETE',command = delete, bg ="#F2E9EB",fg="#647295")
+delete_button.pack( side= RIGHT, anchor ="ne")
 
-linear_button = Checkbutton(window, text = "LINEAR", variale ="x" , onvalue =1 , offvalue =0 , command=linear_search ,font =('Arial',15) ) 
-linear_button.pack(side=LEFT)
+submit_button =Button(window,text='SUBMIT',command = submit, bg ="#F2E9EB",fg="#647295")
+submit_button.pack( side= RIGHT,anchor ="ne")
+# --------------------
+#Query input
+query_= Label(window,text = "INPUT QUERY" ,font=('Arial',18,'bold'), bg ="#D8DFEE" , fg="#647295")
+query_.pack(side= LEFT ,anchor ="e")
+query_search= Entry (window,font=("Arial",18),bg="#A3B4CB",fg="#F4F2F3")
+query_search.pack(side= LEFT ,anchor ="e")
+delete_exit_button =Button(window,text='DELETE',command = delete_query, bg ="#F2E9EB",fg="#647295")
+delete_exit_button.pack( side= RIGHT, anchor ="e")
+query_button =Button(window,text='RUN QUERY',command = submit_query, bg ="#F2E9EB",fg="#647295")
+query_button.pack(side=LEFT, anchor ="e")
 
-binary_button = Checkbutton(window, text = "BINARY", variale ="X" , onvalue =1 , offvalue =0 , command= binary_search,font =('Arial',15) ) 
-binary_button.pack(side=LEFT)
+# --------------------
+# Exit output
+outptt= Label(window,text = "OUTPUT" ,font=('Arial',18,'bold'), bg ="#D8DFEE",fg="#647295")
+outptt.pack(side= LEFT,anchor ="se")
+Exit= Entry (window,font=("Arial",18),bg="#A3B4CB",fg="#F4F2F3")
+Exit.pack(side= LEFT,anchor ="se")
+delete_exit_button =Button(window,text='DELETE',command = delete_exit, bg ="#F2E9EB",fg="#647295")
+delete_exit_button.pack( side= LEFT, anchor ="se")
+# --------------------
+#frames 
+f1=Frame(window)
 
-quick_button = Checkbutton(window, text = "QUICK", variale ="x" , onvalue =1 , offvalue =0 , command= quick_sort,font =('Arial',15) ) 
-quick_button.pack(side=RIGHT)
-
-bubble_button = Checkbutton(window, text = "BUBBLE", variale ="x" , onvalue =1 , offvalue =0 , command=bubble_sort,font =('Arial',15) ) 
-bubble_button.pack(side=RIGHT)
-
-select_button = Checkbutton(window, text = "SELECT", variale ="x" , onvalue =1 , offvalue =0 , command= selection_sort,font =('Arial',15) ) 
-select_button.pack(side=RIGHT)
-#------------------------------------------------------------------------------------------------------------
-window.mainloop()  # this will place window on screen and also look for events in it
+# ------------
+window.mainloop()
